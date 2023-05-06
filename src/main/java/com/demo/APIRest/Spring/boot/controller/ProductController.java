4fbd8aac4d51caecf4bd01b4ba0ec98ac983b38d
joabe <joabe.18@hotmail.com>
2023-05-06 14:06:31 -0300

    findAll function created

--- src/main/java/com/demo/APIRest/Spring/boot/controller/ProductController.java
+++ src/main/java/com/demo/APIRest/Spring/boot/controller/ProductController.java
@@ -1,10 +1,25 @@
 package com.demo.APIRest.Spring.boot.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.APIRest.Spring.boot.entities.Product;
+import com.demo.APIRest.Spring.boot.repositories.ProductRepository;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
-
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@GetMapping
+	public List<Product> findAll(){
+		List<Product> list = productRepository.findAll();
+		return list;
+	}
 }
