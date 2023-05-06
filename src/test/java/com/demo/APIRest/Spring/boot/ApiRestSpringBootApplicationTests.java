a3935178dd056306fbc6941f1bf456a15c24d93c
joabe <joabe.18@hotmail.com>
2023-05-06 10:39:40 -0300

    project created

---
+++ src/test/java/com/demo/APIRest/Spring/boot/ApiRestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.APIRest.Spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
