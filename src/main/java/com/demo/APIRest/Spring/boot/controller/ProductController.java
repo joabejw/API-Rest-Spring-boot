6d1afc8913e51a1c8c0bf283dbd7642a87153c3f
joabe <joabe.18@hotmail.com>
2023-05-08 10:16:05 -0300

    findByID function created

--- src/main/java/com/demo/APIRest/Spring/boot/controller/ProductController.java
+++ src/main/java/com/demo/APIRest/Spring/boot/controller/ProductController.java
@@ -3,12 +3,13 @@ package com.demo.APIRest.Spring.boot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.APIRest.Spring.boot.entities.Product;
-import com.demo.APIRest.Spring.boot.repositories.ProductRepository;
 import com.demo.APIRest.Spring.boot.services.ProductService;
 
 @RestController
@@ -22,5 +23,11 @@ public class ProductController {
 	public List<Product> findAll(){
 		List<Product> list = productServices.findAll();
 		return list;
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Product> findByID(@PathVariable("id") Long id) {
+		Product product = productServices.findByID(id);
+		return ResponseEntity.ok().body(product);
 	}
 }
--- src/main/java/com/demo/APIRest/Spring/boot/services/ProductService.java
+++ src/main/java/com/demo/APIRest/Spring/boot/services/ProductService.java
@@ -1,6 +1,7 @@
 package com.demo.APIRest.Spring.boot.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,11 +11,16 @@ import com.demo.APIRest.Spring.boot.repositories.ProductRepository;
 
 @Service
 public class ProductService {
-	
+
 	@Autowired
 	private ProductRepository productRepository;
-	
-	public List<Product> findAll(){
+
+	public List<Product> findAll() {
 		return productRepository.findAll();
+	}
+
+	public Product findByID(Long id) {
+		Optional<Product> product = productRepository.findById(id);
+		return product.get();
 	}
 }
